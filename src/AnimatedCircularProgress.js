d682955f39d801df3f6e30825794343ea978b16d
Salar Abbasi <salaaar@hamravesh.com>
2017-11-17 00:20:07 +0330

    Fix PropTypes bug

--- src/AnimatedCircularProgress.js
+++ src/AnimatedCircularProgress.js
@@ -1,16 +1,15 @@
 import React from 'react';
-import PropTypes from 'prop-types';
+import { PropTypes } from 'react';
 import { View, Animated } from 'react-native';
 import CircularProgress from './CircularProgress';
 const AnimatedProgress = Animated.createAnimatedComponent(CircularProgress);
 
 export default class AnimatedCircularProgress extends React.Component {
-
   constructor(props) {
     super(props);
     this.state = {
-      chartFillAnimation: new Animated.Value(props.prefill || 0)
-    }
+      chartFillAnimation: new Animated.Value(props.prefill || 0),
+    };
   }
 
   componentDidMount() {
@@ -26,32 +25,24 @@ export default class AnimatedCircularProgress extends React.Component {
   animateFill() {
     const { tension, friction } = this.props;
 
-    Animated.spring(
-      this.state.chartFillAnimation,
-      {
-        toValue: this.props.fill,
-        tension,
-        friction
-      }
-    ).start();
+    Animated.spring(this.state.chartFillAnimation, {
+      toValue: this.props.fill,
+      tension,
+      friction,
+    }).start();
   }
 
   performLinearAnimation(toValue, duration) {
     Animated.timing(this.state.chartFillAnimation, {
       toValue: toValue,
-      duration: duration
+      duration: duration,
     }).start();
   }
 
   render() {
     const { fill, prefill, ...other } = this.props;
 
-    return (
-      <AnimatedProgress
-        {...other}
-        fill={this.state.chartFillAnimation}
-        />
-    )
+    return <AnimatedProgress {...other} fill={this.state.chartFillAnimation} />;
   }
 }
 
@@ -64,10 +55,10 @@ AnimatedCircularProgress.propTypes = {
   tintColor: PropTypes.oneOfType([PropTypes.string, PropTypes.object]),
   backgroundColor: PropTypes.oneOfType([PropTypes.string, PropTypes.object]),
   tension: PropTypes.number,
-  friction: PropTypes.number
-}
+  friction: PropTypes.number,
+};
 
 AnimatedCircularProgress.defaultProps = {
   tension: 7,
-  friction: 10
+  friction: 10,
 };
--- src/CircularProgress.js
+++ src/CircularProgress.js
@@ -1,16 +1,27 @@
 import React from 'react';
-import PropTypes from 'prop-types';
+import { PropTypes } from 'react';
 import { View, Platform } from 'react-native';
-import { Surface, Shape, Path, Group } from '../../react-native/Libraries/ART/ReactNativeART';
+import {
+  Surface,
+  Shape,
+  Path,
+  Group,
+} from '../../react-native/Libraries/ART/ReactNativeART';
 import MetricsPath from 'art/metrics/path';
 
 export default class CircularProgress extends React.Component {
-
   circlePath(cx, cy, r, startDegree, endDegree) {
-
     let p = Path();
     p.path.push(0, cx + r, cy);
-    p.path.push(4, cx, cy, r, startDegree * Math.PI / 180, endDegree * Math.PI / 180, 1);
+    p.path.push(
+      4,
+      cx,
+      cy,
+      r,
+      startDegree * Math.PI / 180,
+      endDegree * Math.PI / 180,
+      1,
+    );
     return p;
   }
 
@@ -25,32 +36,53 @@ export default class CircularProgress extends React.Component {
   }
 
   render() {
-    const { size, width, tintColor, backgroundColor, style, rotation, linecap, children } = this.props;
-    const backgroundPath = this.circlePath(size / 2, size / 2, size / 2 - width / 2, 0, 360 * .9999);
+    const {
+      size,
+      width,
+      tintColor,
+      backgroundColor,
+      style,
+      rotation,
+      linecap,
+      children,
+    } = this.props;
+    const backgroundPath = this.circlePath(
+      size / 2,
+      size / 2,
+      size / 2 - width / 2,
+      0,
+      360 * 0.9999,
+    );
 
-    const fill = this.extractFill(this.props.fill);    
-    const circlePath = this.circlePath(size / 2, size / 2, size / 2 - width / 2, 0, (360 * .9999) * fill / 100);
+    const fill = this.extractFill(this.props.fill);
+    const circlePath = this.circlePath(
+      size / 2,
+      size / 2,
+      size / 2 - width / 2,
+      0,
+      360 * 0.9999 * fill / 100,
+    );
 
     return (
       <View style={style}>
-        <Surface
-          width={size}
-          height={size}>
-          <Group rotation={rotation - 90} originX={size/2} originY={size/2}>
-            <Shape d={backgroundPath}
-                   stroke={backgroundColor}
-                   strokeWidth={width}/>
-            <Shape d={circlePath}
-                   stroke={tintColor}
-                   strokeWidth={width}
-                   strokeCap={linecap}/>
+        <Surface width={size} height={size}>
+          <Group rotation={rotation - 90} originX={size / 2} originY={size / 2}>
+            <Shape
+              d={backgroundPath}
+              stroke={backgroundColor}
+              strokeWidth={width}
+            />
+            <Shape
+              d={circlePath}
+              stroke={tintColor}
+              strokeWidth={width}
+              strokeCap={linecap}
+            />
           </Group>
         </Surface>
-        {
-          children && children(fill)
-        }
+        {children && children(fill)}
       </View>
-    )
+    );
   }
 }
 
@@ -63,12 +95,12 @@ CircularProgress.propTypes = {
   backgroundColor: PropTypes.string,
   rotation: PropTypes.number,
   linecap: PropTypes.string,
-  children: PropTypes.func
-}
+  children: PropTypes.func,
+};
 
 CircularProgress.defaultProps = {
   tintColor: 'black',
   backgroundColor: '#e4e4e4',
   rotation: 90,
-  linecap: 'butt'
-}
+  linecap: 'butt',
+};
